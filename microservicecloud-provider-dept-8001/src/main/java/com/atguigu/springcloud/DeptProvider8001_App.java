5229af6940884ae412c7e2458a064621a1a0177b
liu <1712624418@qq.com>
2020-02-04 00:34:04 +0800

    new

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -2,11 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Created by ZeroV on 2018/11/6.
  */
 
 @SpringBootApplication
+@EnableEurekaClient   //本服启动后会自动注册进eureka服务中
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
